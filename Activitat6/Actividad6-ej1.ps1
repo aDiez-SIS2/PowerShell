10ad6a53aad37d968b2da4b9c01cb49d88f5dc4a
aDiez-SIS2 <adiezrivera.cf@iesesteveterradas.cat>
2023-11-06 19:50:33 +0100

    Afegim les activitats a github. Pugem Activitat 6

---
+++ Activitat6/Actividad6-ej1.ps1
@@ -0,0 +1,10 @@
+﻿$Ruta = "C:\Users"
+$NombreArchivo = Read-Host "Introduce el nombre del archivo o directorio"
+
+$Resultado = Get-ChildItem -Path $Ruta -Recurse | Where-Object { $_.Name -eq $NombreArchivo}
+
+if ($Resultado) {
+    Write-Host "El archivo o directorio '$NombreArchivo' se encontró en la ruta '$Ruta'."
+} else {
+    Write-Host "El archivo o directorio '$NombreArchivo' no se encontró en la ruta '$Ruta'."
+}
---
+++ Activitat6/actividad6-ej2.ps1
@@ -0,0 +1,13 @@
+﻿$carp = Read-Host "Introduce la ruta absoluta del directorio a copiar"
+
+if (Test-Path -Path $carp){
+    $dest = Read-Host "Introduce la ruta absoluta del directorio donde se guardara la copia"
+    if (Test-Path -Path $dest){
+        Copy-Item -Path $carp -Destination $dest -Recurse
+        Write-Host "Copia completada"
+    }else{
+        Write-Host "El directorio de destino no se encuentra"
+    }
+}else{
+    Write-Host "El directorio a copiar no se encuentra"
+}
